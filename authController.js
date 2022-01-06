c549363d34e9b08817096b4a45d20ea12dc5fa97
Kirill Andreev <kenokoz@yandex.ru>
2022-01-06 15:59:10 +0300

    add user registration method

--- authController.js
+++ authController.js
@@ -1,8 +1,24 @@
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
+const Role = require('./models/Role');
+
 class AuthController {
   async registration(req, res) {
     try {
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+
+      if (candidate) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+      const hashPass = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: 'USER' });
+      const user = new User({ username, password: hashPass, roles: [userRole.value] });
+      await user.save();
+      return res.json({ message: 'The user has been successfully registered' });
     } catch (e) {
       console.log(e);
+      res.status(400).json({ message: 'Registration error' });
     }
   }
 
@@ -10,6 +26,7 @@ class AuthController {
     try {
     } catch (e) {
       console.log(e);
+      res.status(400).json({ message: 'Login error' });
     }
   }
 
